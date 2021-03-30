d36dafd80b54fce4ec5eaa644b118482f6d265b6
morioprog <morio.prog@gmail.com>
2021-03-30 23:36:59 +0900

    add Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import "../styles/globals.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;

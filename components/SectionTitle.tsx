960f07a239cff2cac257cff832f873e66234eb9f
morioprog <morio.prog@gmail.com>
2021-04-02 12:31:00 +0900

    change color of SectionTitle border

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,27 +1,33 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, useTheme } from "@chakra-ui/react";
 
 type Props = {
   title: string;
 };
 
-const SectionTitle = ({ title }: Props) => (
-  <Heading
-    as="h2"
-    color="#04041c"
-    fontFamily="Avenir"
-    fontWeight="normal"
-    fontSize={{ base: "24px", sm: "40px", md: "54px" }}
-    margin="32px auto"
-    _after={{
-      display: "block",
-      height: "2px",
-      content: "''",
-      background:
-        "linear-gradient(to right, white 25%, red 40%, blue 60%, white 75%)",
-    }}
-  >
-    {title}
-  </Heading>
-);
+const SectionTitle = ({ title }: Props) => {
+  const theme = useTheme();
+
+  return (
+    <Heading
+      as="h2"
+      color="#04041c"
+      fontFamily="Avenir"
+      fontWeight="normal"
+      fontSize={{ base: "24px", sm: "40px", md: "54px" }}
+      margin="32px auto"
+      _after={{
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        height: "2px",
+        width: "30%",
+        content: "''",
+        background: `linear-gradient(to right, ${theme.colors.mainColor[4]} 0%, ${theme.colors.mainColor[2]} 100%)`,
+      }}
+    >
+      {title}
+    </Heading>
+  );
+};
 
 export default SectionTitle;

3798f43f906e5687c5cc401bf6811304436aec57
morioprog <morio.prog@gmail.com>
2021-04-04 00:44:02 +0900

    add title && ogp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "public/global.css";
 
@@ -16,10 +17,27 @@ const customTheme = extendTheme({ colors });
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={customTheme}>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Shota Iwamoto's Portfolio</title>
+        <meta property="og:title" content="Shota Iwamoto's Portfolio" />
+        <meta
+          property="og:description"
+          content="岩本翔太のポートフォリオサイトです"
+        />
+        <meta
+          property="og:image"
+          content="https://github.com/morioprog/portfolio2/blob/master/public/icon.jpg?raw=true"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@morio_prog" />
+        <meta name="twitter:creator" content="@morio_prog" />
+      </Head>
+      <ChakraProvider theme={customTheme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 

5b3824f8e1ac280c774bc1083b62639bbd1f2693
morioprog <morio.prog@gmail.com>
2021-04-04 01:31:51 +0900

    fix padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,7 @@ const Home: NextPage = () => {
       <Heading />
       <Center bg="mainColor.1">
         <Box
-          paddingTop={{ base: "16px", sm: "32px", md: "48px" }}
-          paddingBottom="128px"
+          paddingBottom="48px"
           width={{ base: "95vw", sm: "90vw", md: "80vw", lg: "70vw" }}
           textAlign="center"
         >
